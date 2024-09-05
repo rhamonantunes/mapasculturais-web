535e4eed3082b5d5418041e29a640b15346f516f
rhamonantunes <66077933+rhamonantunes@users.noreply.github.com>
2024-09-05 20:40:16 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${SERVICE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${MAPASCULTURAIS_SERVICE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
